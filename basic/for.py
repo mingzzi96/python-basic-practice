66474bdd55d69796ebcd9c8bedfbe27bdec3baab
mingming <jimin23623@gmail.com>
2024-05-28 17:09:01 +0900

    first commit

---
+++ basic/for.py
@@ -0,0 +1,12 @@
+fruits= ['사과', '망고', '포도', '딸기']
+
+for fruit in fruits:
+    print(fruit)
+
+ages = [5, 15, 20, 3, 33, 40]
+
+for age in ages:
+    if(age > 20): 
+        print(f"{age}살은 성인입니다.")
+    else: print(f"{age}살은 청소년입니다.")
+
